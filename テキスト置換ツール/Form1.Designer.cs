b2397a49b5a94c19a139e89f5bdc24094320afbe
Akira Nakashima <nakashima@himika.co.jp>
2023-12-05 08:54:23 +0900

    first commit

---
+++ テキスト置換ツール/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace テキスト置換ツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(532, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ディレクトリ選択";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbDirectory
+            // 
+            this.tbDirectory.Location = new System.Drawing.Point(31, 31);
+            this.tbDirectory.Name = "tbDirectory";
+            this.tbDirectory.Size = new System.Drawing.Size(495, 19);
+            this.tbDirectory.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ディレクトリパス";
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(31, 80);
+            this.tbSource.Multiline = true;
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(235, 226);
+            this.tbSource.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "置き換えたいワード（左から右へ、タブ区切り）";
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(393, 80);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(90, 23);
+            this.btnReplace.TabIndex = 6;
+            this.btnReplace.Text = "置き換える";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 342);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbSource);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDirectory);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "置き換えツール";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ テキスト置換ツール/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace テキスト置換ツール
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbDirectory.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            var listWords = new List<(string, string)>();
+            var words = tbSource.Text.Split(new string[] {Environment.NewLine },StringSplitOptions.None);
+            foreach(var word in words)
+            {
+                var split = word.Split('\t');
+                if (split.Length != 2) continue;
+                listWords.Add((split[0],split[1]));
+            }
+
+            var files = Directory.GetFiles(tbDirectory.Text);
+            var new_directory = tbDirectory.Text + "\\" + "置換後";
+            if (!Directory.Exists(new_directory))
+            {
+                Directory.CreateDirectory(new_directory);
+            }
+            foreach (var file in files)
+            {
+                var fileinfo = new FileInfo(file);
+                using (var sw = new StreamWriter(new_directory + "\\" + fileinfo.Name, true, Encoding.GetEncoding("Shift_Jis")))
+                using (var sr = new StreamReader(file, Encoding.GetEncoding("Shift_Jis")))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        var line = sr.ReadLine();
+                        foreach (var word in listWords)
+                        {
+                            line = line.Replace(word.Item1, word.Item2);
+                        }
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+            MessageBox.Show("Done");
+        }
+    }
+}
